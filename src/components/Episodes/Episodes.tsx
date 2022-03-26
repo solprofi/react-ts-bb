e0caf5d7dce1c994b66198e2a97ac46a8792f5fd
Roman Solohor <solohor.roman@gmail.com>
2022-03-26 19:17:49 +0200

    Add episodes page data mapping

--- src/components/Episodes/Episodes.tsx
+++ src/components/Episodes/Episodes.tsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import groupBy from 'lodash-es/groupBy';
+import map from 'lodash-es/map';
+
+import { fetchEpisodes } from '../../api/episodes';
+import SeasonBlock from '../SeasonBlock/SeasonBlock';
+import {
+  Episode, 
+  Season,
+  Seasons,
+} from '../../types/types';
 
 const Episodes = () => {
+  const [groupedEpisodes, setGroupedEpisodes] = useState<Seasons | []>([]);
+  const [isLoading, setIsLoading] = useState<Boolean>(true);
+
+  const fetchEpisodeData = async () => {
+    const data: Episode[]  = await fetchEpisodes();
+
+    const mappedEpisodesBySeason: Seasons = groupBy(data, 'season');
+    setGroupedEpisodes(mappedEpisodesBySeason);
+  }
+  
+  useEffect(() => {
+    fetchEpisodeData();
+    setIsLoading(false);
+  }, []);
+
+  const renderSeasons = () => map(groupedEpisodes, (seasonData: Season, key: string) => {
+      return (
+        <SeasonBlock
+          seasonData={seasonData}
+          key={key} />
+      );
+  });
+  
+
   return (
     <div>
-      Episodes Page
+      {renderSeasons()}
     </div>
   );
 }
