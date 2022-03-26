da8ca0190ced5f37d7a4ef0a4d07d547ecd6629a
Roman Solohor <solohor.roman@gmail.com>
2022-03-26 19:18:12 +0200

    Add season block component skeleton

---
+++ src/components/SeasonBlock/SeasonBlock.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Collapse } from '@mui/material';
+
+import { Season } from '../../types/types';
+
+type SeasonBlockProps = {
+  seasonData: Season
+}
+
+const SeasonBlock = (props: SeasonBlockProps) => {
+  return (
+    <div>
+      SeasonBlock
+    </div>
+  );
+}
+
+export default SeasonBlock;
