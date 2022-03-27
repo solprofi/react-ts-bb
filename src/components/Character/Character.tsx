ad91d016c498306081a243bffb9a42b871a55ef3
Roman Solohor <solohor.roman@gmail.com>
2022-03-27 18:56:33 +0300

    Update character data rendering

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -3,54 +3,128 @@ import React, {
   useEffect,
   useCallback,
 } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardMedia,
+  Skeleton
+} from '@mui/material';
+import map from 'lodash-es/map';
 
-import { PATHS } from '../../constants/paths';
 import { Character } from '../../types/types';
 import { fetchCharacterByName } from '../../api/characters';
 
 const CharacterPage = () => {
   const { name } = useParams();
-  const navigate = useNavigate();
 
   const [characterData, setCharacterData] = useState<Character | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  const redirectHome = useCallback(() => {
-    navigate(PATHS.EPISODES);
-  }, [navigate]);
 
   const fetchCharacterData = useCallback(async () => {
     try {
       if (name) {
         const mappedName = name.replace(' ', '+');
         const data: Character[] = await fetchCharacterByName(mappedName);
-        
+
         if (data.length) {
-            setCharacterData(data[0]);
-        } else {
-          return redirectHome();
+          setCharacterData(data[0]);
         }
       }
     } catch (e) {
-      
-    } finally {
-      setIsLoading(false);
+
     }
-  }, [name, redirectHome]);
+  }, [name]);
 
   useEffect(() => {
-    fetchCharacterData();
-    setIsLoading(false);
-
-    return () => {};
+    fetchCharacterData().then(() => {
+      setIsLoading(false);
+    });
   }, [fetchCharacterData]);
 
-  return (
-    <div>
-      {name}
-    </div>
-  );
+  const renderOccupationList = (occupations: string[]) => {
+    return map(occupations, job => (
+      <Typography
+        variant='body2'
+        color='text.secondary'
+        key={job}
+      >
+        - {job}
+      </Typography>
+    ))
+  }
+
+  if (isLoading) {
+    return (
+      <Skeleton
+        variant='rectangular'
+        width={345}
+        height={450}
+        animation='wave'
+      />
+    )
+  }
+
+  if (characterData) {
+    const {
+      name,
+      birthday,
+      nickname,
+      img,
+      status,
+      occupation
+    } = characterData;
+
+    return (
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component='img'
+          height='450'
+          image={img}
+          alt={name}
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant='h5'
+            component='div'
+          >
+            {name}
+          </Typography>
+          <Typography
+            gutterBottom
+            variant='h6'
+            component='div'
+          >
+            a.k.a. "{nickname}"
+          </Typography>
+          <Typography
+            variant='body2'
+            color='text.secondary'
+          >
+            Status: {status}
+          </Typography>
+          <Typography
+            variant='body2'
+            color='text.secondary'
+          >
+            DOB: {birthday}
+          </Typography>
+
+          <Typography
+            variant='body2'
+            color='text.secondary'
+          >
+            Occupation(s):
+          </Typography>
+          {renderOccupationList(occupation)}
+        </CardContent>
+      </Card>
+    );
+  }
+
+  return <div>There is no data for this character</div>;
 }
 
 export default CharacterPage;
